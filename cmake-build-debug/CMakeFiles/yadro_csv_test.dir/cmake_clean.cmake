e1e3bbb195ce7a080355555b54f9f59d12fffe01
vva07 <vva0779@yandex.ru>
2023-06-03 14:58:36 +0300

    project created

---
+++ cmake-build-debug/CMakeFiles/yadro_csv_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yadro_csv_test.dir/main.cpp.obj"
+  "libyadro_csv_test.dll.a"
+  "yadro_csv_test.exe"
+  "yadro_csv_test.exe.manifest"
+  "yadro_csv_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yadro_csv_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
